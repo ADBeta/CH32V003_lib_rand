5e353ebf8e06e9a4bf535f8daf810e24a93338bf
ADBeta <ashley.adbeta@gmail.com>
2024-08-26 21:25:34 +0100

    Added RANDOM_STRENGTH 2

--- testing/rand-test.c
+++ testing/rand-test.c
@@ -7,6 +7,8 @@
 
 #include <stdio.h>
 
+#define RANDOM_STRENGTH 2
+
 
 uint32_t _lfsr = 0x747AA32F;
 
@@ -14,35 +16,44 @@ uint32_t _lfsr = 0x747AA32F;
 // Calculate the next most-significant-bit from the LFSR
 uint32_t lfsr_next_msb(void)
 {
-	/*
-	// Using bits 31 21 1 0 as feedback taps.
-	uint32_t bit_31 = (_lfsr >> 31) & 0x01;
-	uint32_t bit_21 = (_lfsr >> 21) & 0x01;
-	uint32_t bit_01 = (_lfsr >> 1)  & 0x01;
-	uint32_t bit_00 =  _lfsr        & 0x01;
-
-	//return bit_31 ^ bit_21 ^ bit_01 ^ bit_00;
-	uint32_t new_bit = (bit_31 ^ bit_21 ^ bit_01 ^ bit_00) << 31;
-	_lfsr = (_lfsr >> 1) | new_bit;
-	*/
-
-
-	
 	// Shifting to MSB to make calculations more efficient later
 	uint32_t bit_31 = _lfsr         & 0x80000000;
 	uint32_t bit_21 = (_lfsr << 10) & 0x80000000;
 	uint32_t bit_01 = (_lfsr << 30) & 0x80000000;
 	uint32_t bit_00 = (_lfsr << 31) & 0x80000000;
 
-	//return bit_31 ^ bit_21 ^ bit_01 ^ bit_00;
-	uint32_t new_bit = bit_31 ^ bit_21 ^ bit_01 ^ bit_00;
-	_lfsr = (_lfsr >> 1) | new_bit;
-	
-
-	return new_bit;
+	return bit_31 ^ bit_21 ^ bit_01 ^ bit_00;
 }
 
+uint32_t rand(void)
+{
+	uint32_t r_output = 0;
 
+	// If RANDOM_STRENGTH is level 1, just Shift the register by 1
+	#if RANDOM_STRENGTH == 1
+	_lfsr = (_lfsr >> 1) | lfsr_next_msb();
+	r_output = _lfsr;
+	#endif
+
+	// If RANDOM_STRENGTH is level 2, generate a new bit for all 32bits of
+	// the output variable
+	#if RANDOM_STRENGTH == 2
+	uint8_t bits = 32;
+	while(bits--)
+	{
+		// Get the next bit and update the LFSR
+		uint32_t msb = lfsr_next_msb();
+		_lfsr = (_lfsr >> 1) | msb;
+
+		// Slip the new bit into the output
+		r_output = r_output >> 1;
+		r_output |= msb;
+	}
+
+	#endif
+
+	return r_output;
+}
 
 
 int main() 
@@ -50,10 +61,10 @@ int main()
 	SystemInit();
 
 
-	for(int x = 0; x < 255; x++)
+	for(int x = 0; x < 10; x++)
 	{
 		printf("0x%08X\n",   _lfsr);
-		printf("0x%08X\n\n", lfsr_next_msb());
+		printf("0x%08X\n\n", rand());
 	}
 	return 0;
 }
